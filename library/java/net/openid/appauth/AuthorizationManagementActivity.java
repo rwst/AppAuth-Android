b7db075cc0e7ab87c7685e87754154979e9ad641
Ralf Stephan <ralf@ark.in-berlin.de>
2022-03-11 10:27:12 +0100

    send result code; cosmetics; fixes #818

--- library/java/net/openid/appauth/AuthorizationManagementActivity.java
+++ library/java/net/openid/appauth/AuthorizationManagementActivity.java
@@ -327,15 +327,15 @@ public class AuthorizationManagementActivity extends AppCompatActivity {
         }
     }
 
-    private void sendResult(PendingIntent callback, Intent cancelData, int resultCode) {
+    private void sendResult(PendingIntent callback, Intent data, int resultCode) {
         if (callback != null) {
             try {
-                callback.send(this, 0, cancelData);
+                callback.send(this, resultCode, data);
             } catch (CanceledException e) {
-                Logger.error("Failed to send cancel intent", e);
+                Logger.error("Failed to send intent", e);
             }
         } else {
-            setResult(resultCode, cancelData);
+            setResult(resultCode, data);
         }
     }
 
